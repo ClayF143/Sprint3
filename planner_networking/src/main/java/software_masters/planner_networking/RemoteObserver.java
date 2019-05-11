7fa1921a86cb2f56e338f1403ad9104b5d3ea15d
ClayF143 <48032691+ClayF143@users.noreply.github.com>
2019-05-11 14:14:18 -0400

    Update RemoteObserver.java

--- planner_networking/src/main/java/software_masters/planner_networking/RemoteObserver.java
+++ planner_networking/src/main/java/software_masters/planner_networking/RemoteObserver.java
@@ -2,9 +2,17 @@ package software_masters.planner_networking;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Observable;
 
-public interface RemoteObserver extends Remote {
-	void update(Observable o, Object arg) throws RemoteException;
+public interface RemoteClient extends Remote {
+	
+	void update(String a) throws RemoteException;
+
+	Server getServer() throws RemoteException;
+	
 	String getCookie() throws RemoteException;
+
+	void setObserverID(int id) throws RemoteException;
+	
+	int getObserverID() throws RemoteException;
+
 }
